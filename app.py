38428c6659c22b20bed0e5e2a7b369bf4a983163
Ariq Koh <105147577+sellmenn@users.noreply.github.com>
2024-07-27 20:27:55 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, request
 import telebot
-import openai
+from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,7 +12,7 @@ OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 WEBHOOK_URL = os.environ.get("WEBHOOK_URL")  # Your Heroku app URL with the path to the webhook
 
 tb = telebot.TeleBot(token=TOKEN)
-openai.api_key = OPENAI_API_KEY
+client = OpenAI(api_key=OPENAI_API_KEY)
 
 app = Flask(__name__)
 
@@ -31,7 +31,7 @@ def send_welcome(message):
 @tb.message_handler()
 def handle_message(message):
     try:
-        response = openai.ChatCompletion.create(
+        response = client.ChatCompletion.create(
             model="gpt-4o-mini",
             messages=[
                 {"role": "system", "content": "You are an AI assistant named Asian Fleming, created by Ariq Koh, skilled in explaining Physics concepts under the Singapore H2/H1 A Levels syllabus. Your replies should only be in text with no formatting. Your answers should be relevant to the latest syllabus."},
